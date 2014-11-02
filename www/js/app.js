20f97e5d44df91fed77599d8032880ead872f6ce
Bear Cott <bear@bearcott.com>
2014-11-01 18:44:59 -0700

    cordova camera v1

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic','starter.controllers'])
+angular.module('app', ['ionic','starter.controllers','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -62,7 +62,8 @@ angular.module('app', ['ionic','starter.controllers'])
     
     .state('scantx', {
       url: '/scantx',
-      templateUrl: 'scantx.html'
+      templateUrl: 'scantx.html',
+      controller: 'scantxCtrl'
     })
     
     .state('offlinescan', {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,24 @@
 angular.module('starter.controllers', [])
 
-.controller('sendCtrl', function($scope) {
-  $('#qrscanner').html5_qrcode(function(data){
-           // do something when code is read
-      },
-      function(error){
-          //show read errors 
-      }, function(videoError){
-          //the video stream could be opened
-      }
-  );
+.controller('sendCtrl', function($scope, $cordovaBarcodeScanner) {
+  $scope.scanBarcode = function() {
+      $cordovaBarcodeScanner.scan().then(function(imageData) {
+          alert(imageData.text);
+          console.log("Barcode Format -> " + imageData.format);
+          console.log("Cancelled -> " + imageData.cancelled);
+      }, function(error) {
+          console.log("An error happened -> " + error);
+      });
+  };
+})
+.controller('scantxCtrl', function($scope, $cordovaBarcodeScanner) {
+  $scope.scanBarcode = function() {
+      $cordovaBarcodeScanner.scan().then(function(imageData) {
+          alert(imageData.text);
+          console.log("Barcode Format -> " + imageData.format);
+          console.log("Cancelled -> " + imageData.cancelled);
+      }, function(error) {
+          console.log("An error happened -> " + error);
+      });
+  };
 })
