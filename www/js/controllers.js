3d7df4ee962f37ad465417ec7390b197e30381f6
Bear Cott <bear@bearcott.com>
2014-11-01 19:40:36 -0700

    gotta work camera pls

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngCordova'])
 
 .controller('sendCtrl', function($scope, $cordovaBarcodeScanner) {
   $scope.scanBarcode = function() {
